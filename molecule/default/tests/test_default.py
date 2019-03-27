feacaf0a6273fc3ba9573329abf3403ef0fba5d5
Grigori Py <pygrigori@gmail.com>
2019-03-27 13:54:21 +0300

    molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_pkg_installed(host):
+    package = host.package('haproxy')
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert package.is_installed
+
+
+def test_service_is_enabled(host):
+    service = host.service('haproxy')
+
+    assert service.is_enabled
+
+
+def test_config_exists(host):
+    file = host.file('/etc/haproxy/haproxy.cfg')
+
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+
+
+def test_config_is_valid(host):
+    cmd = host.run('haproxy -c -f /etc/haproxy/haproxy.cfg')
+
+    assert not cmd.rc
